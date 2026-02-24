dc2a506918abe1b701b8c67a769d545713e758e7
Q.H <utii6@iCloud.com>
2026-02-24 16:07:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -138,6 +138,8 @@ def auto_view_posts(message):
 # --- أمر /start (تفاعل 🔥 + إحالات 9) ---
 @bot.message_handler(commands=['start'])
 def start(message):
+     if not security.check_user(bot, message, get_db_connection): return
+
     uid = message.from_user.id
     args = message.text.split()
     
